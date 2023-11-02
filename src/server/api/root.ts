af5153fe0c380ab3119ee85796a864a7512d8be5
Srivatsa <srivatsarupadhya@gmail.com>
2023-11-02 20:23:30 +0530

    remove unnecessary files

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,12 +1,10 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import CollegeRouter from "./routers/colleges.router";
 import TeamRouter from "./routers/teams.router";
 import { adminRouter } from "./routers/admin.router";
 
 export const appRouter = createTRPCRouter({
 	admin: adminRouter,
 	team: TeamRouter,
-	college: CollegeRouter,
 });
 
 // export type definition of API
