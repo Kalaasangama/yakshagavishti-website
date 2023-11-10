7669cf2059dfe0b9f8b14892a6a7299773bd5e15
Srivatsa <srivatsarupadhya@gmail.com>
2023-11-11 00:59:37 +0530

    feat:View team dialog

--- src/components/ViewTeam.tsx
+++ src/components/ViewTeam.tsx
@@ -1,79 +1,78 @@
+import Image from "next/image";
 import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "src/components/ui/table"
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "src/components/ui/table";
+import { api } from "~/utils/api";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
 
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-]
-
-export default function TableDemo() {
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Members</TableHead>
-          <TableHead>Names</TableHead>
-          <TableHead>ID</TableHead>
-          <TableHead className="text-right">Status</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  )
+export default function ViewTeam() {
+	const teamData = api.team.getTeam.useQuery();
+	return (
+		!teamData.isLoading && (
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button variant="outline" className="">
+						View Team
+					</Button>
+				</DialogTrigger>
+				<DialogContent className="bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-gray-950/50 via-slate-900 to-black text-white sm:max-w-[425px]">
+					<DialogHeader>
+						<DialogTitle className="text-center">View Team</DialogTitle>
+						<DialogDescription className="text-center">
+							View your team details here.
+						</DialogDescription>
+					</DialogHeader>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[100px]">
+									Name
+								</TableHead>
+								<TableHead>ID</TableHead>
+								<TableHead>Status</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{teamData?.data?.members?.map((member, key) => (
+								<TableRow key={key}>
+									<TableCell className="font-medium">
+										{member.name}
+									</TableCell>
+									<TableCell>
+									<a href={member.idURL}>
+										<Image
+											src={member.idURL}
+											alt="id_image"
+											height={100}
+											width={100}
+										/>
+										</a>
+									</TableCell>
+									<TableCell>
+										{member.isIdVerified
+											? "Verified"
+											: "Pending"}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+					<Button>Request Edit</Button>
+				</DialogContent>
+			</Dialog>
+		)
+	);
 }
